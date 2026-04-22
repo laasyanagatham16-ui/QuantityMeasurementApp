1b3b7efb68ac6e2beb581faabc3fdaf1f26f72cc
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-04-22 10:41:41 +0530

    UC1 and UC2 completed

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,4 +1,64 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class QuantityMeasurementApp {
+
+    // 🔹 FEET CLASS
+    static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Feet feet = (Feet) obj;
+            return Double.compare(feet.value, value) == 0;
+        }
+    }
+
+    // 🔹 INCHES CLASS
+    static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+    }
+
+    // 🔹 METHOD FOR FEET EQUALITY
+    public static boolean checkFeetEquality(double v1, double v2) {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+        return f1.equals(f2);
+    }
+
+    // 🔹 METHOD FOR INCHES EQUALITY
+    public static boolean checkInchesEquality(double v1, double v2) {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+        return i1.equals(i2);
+    }
+
+    // 🔹 MAIN METHOD (TESTING)
+    public static void main(String[] args) {
+
+        // UC1: Feet equality
+        System.out.println("Feet Equal (1.0, 1.0): " + checkFeetEquality(1.0, 1.0));
+        System.out.println("Feet Equal (1.0, 2.0): " + checkFeetEquality(1.0, 2.0));
+
+        // UC2: Inches equality
+        System.out.println("Inches Equal (1.0, 1.0): " + checkInchesEquality(1.0, 1.0));
+        System.out.println("Inches Equal (1.0, 2.0): " + checkInchesEquality(1.0, 2.0));
+    }
 }
